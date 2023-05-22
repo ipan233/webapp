b211765dc2f73d1bba02ad599c05cce65d2be183
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2023-05-22 23:26:14 +0800

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-import time
-cmd = "chmod +x /start.sh && /start.sh"
-subprocess.call(cmd, shell=True)
-cmd2 = "chmod +x /tmp/chatdog.sh && /tmp/chatdog.sh"
-while True:
-  subprocess.call(cmd2, shell=True)
-  time.sleep(20)
